90c65e7f6ba70d11a3d266c504fa2f1c617741e3
RidgelineFire <99427032+dcolbourne@users.noreply.github.com>
2025-02-01 16:48:41 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
-// document.querySelector("button").addEventListener("click", function () {
-//     alert("I got clicked!");
-// });
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-// document.querySelector(".w").addEventListener("click", function () {
-//     var crashAudio = new Audio("./sounds/crash.mp3");
-//     crashAudio.play();
-// });
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+});
+}
 
-// document.querySelector(".a").addEventListener("click", function () {
-//     var kickBassAudio = new Audio("./sounds/kick-bass.mp3");
-//     kickBassAudio.play();
-// });
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
 
-// document.querySelector(".s").addEventListener("click", function () {
-//     var snareAudio = new Audio("./sounds/snare.mp3");
-//     snareAudio.play();
-// });
-
-// document.querySelector(".d").addEventListener("click", function () {
-//     var tom1Audio = new Audio("./sounds/tom-1.mp3");
-//     tom1Audio.play();
-// });
-
-// document.querySelector(".j").addEventListener("click", function () {
-//     var tom2Audio = new Audio("./sounds/tom-2.mp3");
-//     tom2Audio.play();
-// });
-
-// document.querySelector(".k").addEventListener("click", function () {
-//     var tom3Audio = new Audio("./sounds/tom-3.mp3");
-//     tom3Audio.play();
-// });
-
-// document.querySelector(".l").addEventListener("click", function () {
-//     var tom4Audio = new Audio("./sounds/tom-4.mp3");
-//     tom4Audio.play();;
-// });
-
-
-var drumSelect = 0;
-var audioPath = "./sounds/";
-
-var numDrums = document.querySelectorAll(".drum").length;
-
-for (var i = 0; i < numDrums; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var crashAudio = new Audio("./sounds/crash.mp3");
-        crashAudio.play();    
+function makeSound(key) {
+    switch (key) {
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "f":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+        var kickBass = new Audio("./sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+        default:
+            console.log();
+            break;
     }
-}   
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+    
